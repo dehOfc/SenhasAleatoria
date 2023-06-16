0caf3ca0731f46862e8a04b131788a2b27815a09
André Nascimento Costa <nascimentoandre448@gmail.com>
2023-06-16 08:58:52 -0300

    Adicionar arquivos de projeto.

---
+++ SenhasAleatoria/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public class PasswordGenerator
+{
+    private const string Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+
+    public static string GeneratePassword(int length)
+    {
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            var bytes = new byte[length];
+            rng.GetBytes(bytes);
+
+            var password = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                password[i] = Characters[bytes[i] % Characters.Length];
+            }
+
+            return new string(password);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o tamanho da senha desejada:");
+        int length = int.Parse(Console.ReadLine());
+
+        string password = PasswordGenerator.GeneratePassword(length);
+        Console.WriteLine("Senha gerada: " + password);
+    }
+}
